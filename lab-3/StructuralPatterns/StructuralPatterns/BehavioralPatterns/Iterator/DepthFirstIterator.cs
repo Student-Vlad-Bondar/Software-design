729a3b99492e4842a392beeb553c9b7cac857027
VladBondar <ipz235_bvv@student.ztu.edu.ua>
2025-05-06 17:55:33 +0300

    Add Iterator pattern (DFS & BFS) for LightNode traversal

---
+++ lab-3/StructuralPatterns/StructuralPatterns/BehavioralPatterns/Iterator/DepthFirstIterator.cs
@@ -0,0 +1,27 @@
+﻿using StructuralPatterns.Composite;
+
+namespace StructuralPatterns.BehavioralPatterns.Iterator
+{
+    public class DepthFirstIterator : ILightNodeIterator
+    {
+        private readonly Stack<LightNode> _stack = new Stack<LightNode>();
+
+        public DepthFirstIterator(LightNode root)
+        {
+            _stack.Push(root);
+        }
+
+        public bool HasNext() => _stack.Count > 0;
+
+        public LightNode Next()
+        {
+            var node = _stack.Pop();
+            if (node is LightElementNode elem)
+            {
+                for (int i = elem.Children.Count - 1; i >= 0; i--)
+                    _stack.Push(elem.Children[i]);
+            }
+            return node;
+        }
+    }
+}
